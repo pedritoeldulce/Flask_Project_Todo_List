a2afea8b53f8a64b748bf2e1f1cbc2c68ed018c9
pedritoeldulce <franceskolyperez@gmail.com>
2021-03-20 11:29:13 -0500

    Integraciòn con Bootstrap
    Este contiene archivos base sobre el cual se harà heradarà
    los siguientes templates

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ pip install ->:
 
 - Flask-Script: Tiene la funcion "Manager"
   
-
+- Flask-Bootstrap4: librerìa Frontend, este contiene un template base, mas detalles en https://pythonhosted.org/Flask-Bootstrap/basic-usage.html
 
 __Manager__: permite levantar una aplicacion a traves de una instancia, permite definir las url a utilizar
 
--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
-from .views import page # Importamos todas las rutas almancenadas en page
+from .views import page  # Importamos todas las rutas almancenadas en page
+from flask_bootstrap import Bootstrap  # importamos la libreria Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap()  # creamos una instancia de bootstrap
 
 
 def create_app(config):  # Indicamos que la funcion create_app reciba un parametro: config
     app.config.from_object(config)  # Indicamos al aervidor que utilice nuestras configuraciones de 'config'
-    app.register_blueprint(page) # vamos a indicar al servidor con que rutas se va a trabajar
+    bootstrap.init_app(app)  # Le indicamos al servidor que use bootstrap, esto se harà en los templates (herencia)
+    app.register_blueprint(page)  # vamos a indicar al servidor con que rutas se va a trabajar
     return app
