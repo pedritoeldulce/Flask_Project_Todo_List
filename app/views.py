81a6c8142cd8a3890b44e8befa3a8cdc4cd60ab7
pedritoeldulce <franceskolyperez@gmail.com>
2021-03-20 10:29:58 -0500

    Manejando errores
    error 404: page_not_found

--- app/views.py
+++ app/views.py
@@ -5,6 +5,10 @@ from flask import render_template
 page = Blueprint('page', __name__) #(nombre del contexto, contexto en el cual se crea la instancia)
 
 
+@page.app_errorhandler(404) #Error de pàgina no encontrada
+def page_not_found(error):
+    return render_template('errors/404.html'), 404
+
 
 @page.route('/')
 def index():
