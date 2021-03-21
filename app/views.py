37fbc923e2d82aef5705717da7520f75307ba4e3
pedritoeldulce <franceskolyperez@gmail.com>
2021-03-21 14:32:47 -0500

    Pintar el Formulario
    HAcemos uso del WTForm, que genera y valida los inputs

--- app/views.py
+++ app/views.py
@@ -2,10 +2,12 @@ from flask import Blueprint
 from flask import render_template
 
 
-page = Blueprint('page', __name__) #(nombre del contexto, contexto en el cual se crea la instancia)
+from forms import LoginForm  # Importamos el LoginForm
+
+page = Blueprint('page', __name__)  #(nombre del contexto, contexto en el cual se crea la instancia)
 
 
-@page.app_errorhandler(404) #Error de pàgina no encontrada
+@page.app_errorhandler(404)  #Error de pàgina no encontrada
 def page_not_found(error):
     return render_template('errors/404.html'), 404
 
@@ -13,3 +15,9 @@ def page_not_found(error):
 @page.route('/')
 def index():
     return render_template('index.html', title="Index")
+
+
+@page.route('/login')
+def login():
+    form = LoginForm()  # instanciamos form y lo enviamos al template
+    return render_template('auth/login.html', title='Login', form=form)
