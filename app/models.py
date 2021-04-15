de0c281595bfa4c7cfb35c1b518ee6dc5c732502
pedritoeldulce <franceskolyperez@gmail.com>
2021-04-14 20:32:16 -0500

    Generar password
    Se generò una contraseña usando werbzeug
    en este caso tiene codigo hash - 93 digitos

--- app/models.py
+++ app/models.py
@@ -1,15 +1,26 @@
 from . import db
 import datetime
 
+from werkzeug.security import  generate_password_hash
+
 
 class Users(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(93), nullable=False)
+    # password = db.Column(db.String(93), nullable=False)
+    encrypted_password = db.Column(db.String(93), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.datetime.now())
 
+    @property
+    def password(self):
+        pass
+
+    @password.setter
+    def password(self, value):
+        self.encrypted_password = generate_password_hash(value)
+    
     def __str__(self):
         return self.username
 
